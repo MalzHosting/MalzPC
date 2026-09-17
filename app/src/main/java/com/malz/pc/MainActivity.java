cf01e354e16eec4df00cd7bb48b4d26d04ee69f2
MalzHosting <41898282+MalzHosting@users.noreply.github.com>
2026-09-17 12:27:48 +0000

    add Android file picker support

--- app/src/main/java/com/malz/pc/MainActivity.java
+++ app/src/main/java/com/malz/pc/MainActivity.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
     private WebView webView;
+    private ValueCallback<android.net.Uri[]> filePathCallback;
+    private static final int FILE_CHOOSER_REQUEST = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +43,62 @@ public class MainActivity extends Activity {
         settings.setDatabaseEnabled(true);
         settings.setBuiltInZoomControls(false);
         settings.setDisplayZoomControls(false);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
 
         webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(
+                    WebView webView,
+                    ValueCallback<android.net.Uri[]> filePathCallback,
+                    FileChooserParams fileChooserParams) {
+
+                if (MainActivity.this.filePathCallback != null) {
+                    MainActivity.this.filePathCallback.onReceiveValue(null);
+                }
+
+                MainActivity.this.filePathCallback = filePathCallback;
+
+                try {
+                    Intent intent = fileChooserParams.createIntent();
+                    startActivityForResult(intent, FILE_CHOOSER_REQUEST);
+                    return true;
+                } catch (Exception e) {
+                    MainActivity.this.filePathCallback = null;
+                    return false;
+                }
+            }
+        });
 
         setContentView(webView);
 
         webView.loadUrl("https://fare-per-cars-quantity.trycloudflare.com");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == FILE_CHOOSER_REQUEST) {
+            if (filePathCallback == null) {
+                return;
+            }
+
+            android.net.Uri[] results = null;
+
+            if (resultCode == RESULT_OK && data != null) {
+                android.net.Uri uri = data.getData();
+
+                if (uri != null) {
+                    results = new android.net.Uri[]{uri};
+                }
+            }
+
+            filePathCallback.onReceiveValue(results);
+            filePathCallback = null;
+        }
     }
 
     @Override
